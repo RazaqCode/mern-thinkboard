ecabf00bcb8277e085e1e3cc8b011d367d910c81
RazaqCode <razaqrealm@gmail.com>
2025-06-17 10:52:15 +0530

    put, delete

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ app.post("/api/notes", (req, res) =>{
     res.status(201).json({message:"Note created successfully!"});
 });
 
+app.put("/api/notes", (req,res)=>{
+    res.status(200).json({message: "Note updated successfully!"});
+});
+
+app.delete("/api/notes", (req, res)=>{
+    res.status(200).json({mrssage:"Note deleted successfully!"});
+});
+
+
 app.listen(5001, () =>{
     console.log("server started on PORT:5001");
 });
