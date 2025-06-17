0e9b040d455fbad3505c60ea1f7fd5e22559a630
RazaqCode <razaqrealm@gmail.com>
2025-06-17 10:45:46 +0530

    get and post

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const app = express ();
 app.get("/api/notes",(req, res)=>{
     res.send("you got 500 notes");
 });
+ 
+app.post("/api/notes", (req, res) =>{
+    res.status(201).json({message:"Note created successfully!"});
+});
 
 app.listen(5001, () =>{
     console.log("server started on PORT:5001");
